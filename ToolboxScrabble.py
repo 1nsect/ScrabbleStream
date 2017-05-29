ea6a7c2c196e3e4f6e4a733d1c4bb524ed10763c
1nsect <nicolas.krischer@gmail.com>
2017-05-29 22:24:35 +0200

    Have the 4 points of the board on the right order

--- ToolboxScrabble.py
+++ ToolboxScrabble.py
@@ -13,7 +13,7 @@ def ShowImage(title,im,time):
   cv2.destroyAllWindows()
   return;
 
-def CropBoard( image, FillSize, SizeOfReworkedImage, KernelShape, showImageTime ):
+def CropBoard( image, FillSize, KernelShape, showImageTime ):
   "Takes the image to find the outer edges, KernelShape=0 -> Ellipse; KernelShape=1 -> Square "
   
   #blur with a square of 9 (recommanded) and a sigma value of 75 (not strong, not too smooth)
@@ -36,40 +36,73 @@ def CropBoard( image, FillSize, SizeOfReworkedImage, KernelShape, showImageTime 
   #apply Canny Edge algorythm
   canny = cv2.Canny(opening,100,200)
 
-  '''
-  #Find the outer contour in the opened image and retain only useful points
-  contours = cv2.findContours(canny,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-  '''
+  #Find the outer contour in the opened image and retain only useful points - we copy the image because findcontours
+  #is destructive
+  (contours, _) = cv2.findContours(canny.copy(),cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
 
-  #Find the outer contour in the opened image and retain only useful points
-  contours = cv2.findContours(canny,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-
-  largestArea = 0
-
-  #find the biggest contour in contours
-  for i in contours
-    print cv2.contourArea(i)
-
-  #Get first contour (maybe have a function that founds the largest contour instead of picking the first one???)
-  contour = contours[0][0]
-
-  #calculate the corners with the contour list
-  epsilon = 0.1*cv2.arcLength(contour,True)
-  approx = cv2.approxPolyDP(contour,epsilon,True)
+  #sort the contours so that we have the largest contour
+  contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
   
-  #print approx
-  #Define approx as float32 and define size of the perspectived/croped image
-  pts1 = np.float32(approx)
-  pts2 = np.float32([[0,0],[SizeOfReworkedImage,0],[SizeOfReworkedImage,SizeOfReworkedImage],[0,SizeOfReworkedImage]])
-  
-  #Create matrix of perspective transformation
-  Correction = cv2.getPerspectiveTransform(pts1,pts2)
-  
-  #Transform image to get right perspective
-  perspective = cv2.warpPerspective(image,Correction,(SizeOfReworkedImage,SizeOfReworkedImage))
-  
-  
+  #initialize the board contour
+  contour = None
 
+  # loop over our contours
+  for c in contours:
+    # approximate the contour
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+    
+    # if our approximated contour has four points, then
+    # we can assume that we have found our screen
+    if len(approx) == 4:
+      contour = approx
+      break
+
+  # now that we have our screen contour, we need to determine
+  # the top-left, top-right, bottom-right, and bottom-left
+  # points so that we can later warp the image -- we'll start
+  # by reshaping our contour to be our finals and initializing
+  # our output rectangle in top-left, top-right, bottom-right,
+  # and bottom-left order
+  pts = contour.reshape(4, 2)
+  rect = np.zeros((4, 2), dtype = "float32")
+
+  # the top-left point has the smallest sum whereas the bottom-right has the largest sum
+  s = pts.sum(axis = 1)
+  rect[0] = pts[np.argmin(s)]
+  rect[2] = pts[np.argmax(s)]
+  
+  # compute the difference between the points -- the top-right
+  # will have the minumum difference and the bottom-left will
+  # have the maximum difference
+  diff = np.diff(pts, axis = 1)
+  rect[1] = pts[np.argmin(diff)]
+  rect[3] = pts[np.argmax(diff)]
+
+  # now that we have our rectangle of points, let's compute
+  # the width of our new image
+  (tl, tr, br, bl) = rect
+  widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+  widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+
+  # ...and now for the height of our new image
+  heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+  heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+   
+  # take the maximum of the width and height values to reach
+  # our final dimensions
+  maxWidth = max(int(widthA), int(widthB))
+  maxHeight = max(int(heightA), int(heightB))
+
+  # construct our destination points which will be used to
+  # map the screen to a top-down, "birds eye" view
+  dst = np.array([[0, 0],[maxWidth - 1, 0],[maxWidth - 1, maxHeight - 1],[0, maxHeight - 1]], dtype = "float32")
+ 
+  # calculate the perspective transform matrix and warp
+  # the perspective to grab the screen
+  CorrectionMatrix = cv2.getPerspectiveTransform(rect, dst)
+  perspective = cv2.warpPerspective(image, CorrectionMatrix, (maxWidth, maxHeight))
+  
   return perspective
 
 #get X coordinates of all the column
@@ -90,7 +123,7 @@ def getFillingMatrix():
  return 0
 
 
-def getNeiborhood(img,x,y,sizex,sizey,origin):
+def isCellOccupied(img,x,y,sizex,sizey,threshold):
 
   out = np.empty((sizex, sizey))
   
@@ -99,10 +132,8 @@ def getNeiborhood(img,x,y,sizex,sizey,origin):
     for j in range(0,sizey):
       #print img[x+i][y+j]
       out[i][j] = img[x+i][y+j]
-     
-
-  return out.astype(np.uint8)
-
+  
+  return int( out.mean() > threshold)
 
 def getChar(im):
 
@@ -121,40 +152,37 @@ def getChar(im):
 
   ShowImage('caca',erosion,0)
 
-    # Write the image after apply opencv to do some ...
+  # Write the image after apply opencv to do some ...
   cv2.imwrite("temp_char.png", erosion)
 
-    # Recognize text with tesseract for python
+  # Recognize text with tesseract for python
   result = pytesseract.image_to_string(Image.open('temp_char.png'))
 
   return result[0]
 
 
-def getFilledCells(board,positionvector,sizecellx,sizecelly,threshold):
+def getFilledCells(board,positionVector,boardState,sizecellx,sizecelly,threshold):
 
+  #create support matrix of 0
   filledmatrix=np.zeros((15, 15), dtype=int)
   
-
+  #browse the board
   for i in range (0,14):
     for j in range (0,14):
-
-      c = getNeiborhood(board,positionvector[i][0],positionvector[j][0],sizecellx,sizecelly,0)
-      
-      
-      if int(c.mean()) > threshold: 
-        filledmatrix[i][j] = int(0)
-
-      else:
-        filledmatrix[i][j] = int(1)
-    
-
-
-  return fillmatrix 
+      #check if the cell was already processed
+      if(boardState[i][j]==0):
+      #check if the cell is occupied or not
+        if( isCellOccupied(board,positionVector[i][0],positionVector[j][0],sizecellx,sizecelly,threshold) ):
+          filledmatrix[i][j] = int(1)
+        else:
+          filledmatrix[i][j] = int(0)
+  
+  return filledmatrix 
 
 
 def takePicture():
   camera_port = 1
- 
+
 #Number of frames to throw away while the camera adjusts to light levels
   ramp_frames = 30
  
--- main.py
+++ main.py
@@ -12,17 +12,22 @@ import cv2
 import math #to use absolute value function 'fabs'
 import pytesseract
 from PIL import Image
+#from pyimagesearch import imutils
 
 import ToolboxScrabble as ts
 
+#Setting - Setting - Setting - Setting - Setting - Setting - Setting - Setting - Setting - Setting - Setting - 
+
 FirstKernelSize = 10 #Remplissage du plateau
-SecondKernelSize = 30 #Remplissage du plateau
-OutputSize = 500
+SecondKernelSize = 10 #Remplissage du plateau
+ImageSize = 300 #size of the board's image
 
 EdgeProportion = float(90)/float(96)
-TimeToSkip= 100
+TimeToSkip= 0
 TimeToWait = 0
 
+
+#Init - Init - Init - Init - Init - Init - Init - Init - Init - Init - Init - Init - Init - Init - Init - Init - 
 '''Take picture from camera
 im=ts.takePicture()
 im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
@@ -30,30 +35,44 @@ im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 ts.ShowImage('coucou',im,0)
 '''
 
+#Create the survey matrix
+boardState = np.zeros((15, 15), dtype=int)
+
+# load the query image, compute the ratio of the old height
+# to the new height, clone it, and resize it
 im = cv2.imread('PlateauO.jpg',0)
+ratio = im.shape[0] / 300.0
+orig = im.copy()
+im = imutils.resize(im, height = ImageSize)
+
 ts.ShowImage('title',im,TimeToSkip)
 
-perspective = ts.CropBoard(im, FirstKernelSize, OutputSize, 0, TimeToSkip)
+#first croping
+perspective = ts.CropBoard(im, FirstKernelSize, 0, TimeToSkip)
 
-ts.ShowImage('title',perspective,TimeToWait)
+#ts.ShowImage('title',perspective,TimeToWait)
 
-perspective = ts.CropBoard(perspective, SecondKernelSize, OutputSize, 1, TimeToSkip)
+#second croping
+perspective = ts.CropBoard(perspective, SecondKernelSize, 1, TimeToSkip)
 
-ts.ShowImage('Perspective',perspective,TimeToWait)
+#ts.ShowImage('Perspective',perspective,TimeToWait)
 
 
 #get coordinates of all the columns
 X_ = ts.getColumnsCoordinates(EdgeProportion,OutputSize)
 
+#Loop - Loop - Loop - Loop - Loop - Loop - Loop - Loop - Loop - Loop - Loop - Loop - Loop - Loop - Loop - Loop - 
 
-matrix = ts.getFilledCells(perspective,X_,25,25,105)
+#Scan the new board state and extract new caramels
+newBoardState = ts.getFilledCells(perspective,X_,boardState,25,25,105)
 
+#add the new caramels to the boardState matrix
+newBoardState = newBoardState - boardState
+boardState = newBoardState
 
-print matrix
-
-cv2.line(perspective, (X_[0][0],X_[0][0]), (X_[10][0],X_[0][0]), (0,0,0), 2)
-
-ts.ShowImage('Quadrillage',perspective,TimeToWait)
+#draw line to know where the columns are
+#cv2.line(perspective, (X_[0][0],X_[0][0]), (X_[10][0],X_[0][0]), (0,0,0), 2)
+#ts.ShowImage('Quadrillage',perspective,TimeToWait)
 
 cell = ts.getNeiborhood(perspective,X_[7][0]+5,X_[7][0]+5,20,20,0)
 
